f5c13ce5e72da30a741f574fe47bb981d613cd4c
NajamSR <114600653+NajamSR@users.noreply.github.com>
2022-11-03 06:21:14 -0400

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    private double val1;
+    private double val2;
+
+    public Calculator(double number1, double number2) {
+        val1 = number1;
+        val2 = number2;
+    }
 }
