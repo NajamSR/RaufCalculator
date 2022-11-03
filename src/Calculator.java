27c24cee2036e119c22dc38025d54cbe9d696e70
NajamSR <114600653+NajamSR@users.noreply.github.com>
2022-11-03 12:04:05 -0400

    finished (I think)

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,4 +6,59 @@ public class Calculator {
         val1 = number1;
         val2 = number2;
     }
+
+
+    public void Multiply() {
+        double answer = val1 * val2;
+        if (answer % 1 == 0) {
+            System.out.println((int) answer);
+        } else {
+            System.out.println(answer);
+        }
+    }
+
+    public void Divide() {
+        double answer = val1 / val2;
+        if (answer % 1 == 0) {
+            System.out.println((int) answer);
+        } else {
+            System.out.println(answer);
+        }
+    }
+
+    public void Add() {
+        double answer = val1 + val2;
+        if (answer % 1 == 0) {
+            System.out.println((int) answer);
+        } else {
+            System.out.println(answer);
+        }
+    }
+
+    public void Subtract() {
+        double answer = val1 - val2;
+        if (answer % 1 == 0) {
+            System.out.println((int) answer);
+        } else {
+            System.out.println(answer);
+        }
+    }
+
+    public void Remainder() {
+        double answer = val1 % val2;
+        if (answer % 1 == 0) {
+            System.out.println((int) answer);
+        } else {
+            System.out.println(answer);
+        }
+    }
+
+    public void Exponent() {
+        double answer = Math.pow(val1, val2);
+        if (answer % 1 == 0) {
+            System.out.println((int) answer);
+        } else {
+            System.out.println(answer);
+        }
+    }
 }
