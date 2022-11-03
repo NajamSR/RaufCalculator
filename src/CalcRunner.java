684a2272ce1be86de3ca2fb543edeb3224d2bc3f
NajamSR <114600653+NajamSR@users.noreply.github.com>
2022-11-03 12:02:55 -0400

    finished (I think)

--- src/CalcRunner.java
+++ src/CalcRunner.java
@@ -2,16 +2,35 @@ import java.util.Scanner;
 
 public class CalcRunner {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        double num1, num2;
-        String operator;
-        System.out.println("Enter an expression containing *, /, +, -, %, or ^: ");
+
+        System.out.println("Enter an expression with *, /, +, -, %, or ^: ");
         String expression = scan.nextLine();
 
-        num1 = Double.parseDouble(expression.substring(0, expression.indexOf(" ")));
-        operator = expression.substring(expression.indexOf(" ") + 1, expression.indexOf(" ") + 2);
-        num2 = Double.parseDouble(expression.substring(expression.indexOf(" ") + 2));
+        double num1 = Double.parseDouble(expression.substring(0, expression.indexOf(" ")));
+        String operator = expression.substring(expression.indexOf(" ") + 1, expression.indexOf(" ") + 2);
+        double num2 = Double.parseDouble(expression.substring(expression.indexOf(" ") + 3));
 
         Calculator calc = new Calculator(num1, num2);
+
+        if (operator.equals("*")) {
+            calc.Multiply();
+        }
+        if (operator.equals("/")) {
+            calc.Divide();
+        }
+        if (operator.equals("+")) {
+            calc.Add();
+        }
+        if (operator.equals("-")) {
+            calc.Subtract();
+        }
+        if (operator.equals("%")) {
+            calc.Remainder();
+        }
+        if (operator.equals("^")) {
+            calc.Exponent();
+        }
     }
 }
