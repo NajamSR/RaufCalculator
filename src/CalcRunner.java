64b3dfcabdd1eaa61f62dd365134cf0e6c883430
NajamSR <114600653+NajamSR@users.noreply.github.com>
2022-11-03 06:37:02 -0400

    Update CalcRunner.java

--- src/CalcRunner.java
+++ src/CalcRunner.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class CalcRunner {
+import java.util.Scanner;
+
+public class CalcRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double num1, num2;
+        String operator;
+        System.out.println("Enter an expression containing *, /, +, -, %, or ^: ");
+        String expression = scan.nextLine();
+
+        num1 = Double.parseDouble(expression.substring(0, expression.indexOf(" ")));
+        operator = expression.substring(expression.indexOf(" ") + 1, expression.indexOf(" ") + 2);
+        num2 = Double.parseDouble(expression.substring(expression.indexOf(" ") + 2));
+
+        Calculator calc = new Calculator(num1, num2);
+    }
 }
